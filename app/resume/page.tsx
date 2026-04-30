a33bd98ba3ae3684e7ae859d7a838a94dcd40bd4
gmartinez78 <greddysmartinez5@gmail.com>
2026-04-30 21:02:36 +0200

    Add downloadable resume PDF

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -451,7 +451,7 @@ export default function ResumePage() {
             size="sm"
             className="shrink-0"
           >
-            <a href="#">
+            <a href={withBasePath("/documents/greddys-martinez-resume-2026.pdf")} download>
               <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" y1="15" x2="12" y2="3"/></svg>
               Download PDF
             </a>
